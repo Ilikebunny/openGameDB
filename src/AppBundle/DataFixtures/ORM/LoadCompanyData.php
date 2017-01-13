9ae7e4fb33ee60bcbec11a2ee71f9f757466d4fa
Ilikebunny <larrue.jerome@gmail.com>
2017-01-13 21:09:23 +0100

    Added Data Fixtures - Company

---
+++ src/AppBundle/DataFixtures/ORM/LoadCompanyData.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AppBundle\Entity\Company;
+
+class LoadCompanyData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager) {
+
+        $manager->getConnection()->getConfiguration()->setSQLLogger(null);
+        $import = $this->container->get('AppBundle.importcsv');
+        $fileContent = $import->CSV_to_array('company.csv');
+
+        $batchSize = 20;
+        $i = 1;
+        foreach ($fileContent as $numRow => $row) {
+            if ($numRow != 1) {
+               
+                $i = $i + 1 ;
+                $entity = new Company();
+
+                $entity->setName($row[0]);
+
+                $manager->persist($entity);
+
+                if (($i % $batchSize) === 0) {
+                    $manager->flush();
+                    $manager->clear(); // Detaches all objects from Doctrine!
+                }
+            }
+        }
+        $manager->flush(); //Persist objects that did not make up an entire batch
+        $manager->clear();
+    }
+
+    public function getOrder() {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 1;
+    }
+
+}
