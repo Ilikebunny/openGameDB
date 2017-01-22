3bf40046a9eccd428abbe8726f69bde3a6382550
Ilikebunny <larrue.jerome@gmail.com>
2017-01-22 14:43:30 +0100

    added game / developper fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadGame_DevelopperData.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AppBundle\Entity\Game;
+use AppBundle\Entity\Company;
+
+class LoadGame_DevelopperData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface {
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager) {
+
+        $manager = $this->container->get('doctrine')->getEntityManager('default');
+
+        $manager->getConnection()->getConfiguration()->setSQLLogger(null);
+        $import = $this->container->get('AppBundle.importcsv');
+        $fileContent = $import->CSV_to_array('game_developper.csv');
+
+        $batchSize = 20;
+        $i = 1;
+        foreach ($fileContent as $numRow => $row) {
+            if ($numRow != 1) {
+
+                $i = $i + 1;
+
+                $entity = $this->getReference("Game_" . $row[0]);
+
+                $entity2 = $this->getReference("Company_" . $row[1]);
+                
+                $entity->addDeveloper($entity2);
+
+                $manager->persist($entity);
+
+                if (($i % $batchSize) === 0) {
+                    $manager->flush();
+                    $manager->clear(); // Detaches all objects from Doctrine!
+                }
+            }
+        }
+        $manager->flush(); //Persist objects that did not make up an entire batch
+        $manager->clear();
+    }
+
+    public function getOrder() {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 9;
+    }
+
+}
