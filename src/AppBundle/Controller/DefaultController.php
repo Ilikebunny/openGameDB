4c28b65acc426df628dc8ffed9f1c3002cc3a401
Ilikebunny <larrue.jerome@gmail.com>
2017-02-03 08:03:41 +0100

    test elastica

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,21 +6,27 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        
+    public function indexAction(Request $request) {
+
         $import = $this->container->get('AppBundle.importcsv');
         $fileContent = $import->CSV_to_array('platform.csv');
-        
+
+        $finder = $this->container->get('fos_elastica.finder.opengamedb.game');
+
+// Option 1. Returns all users who have example.net in any of their mapped fields
+        $results = $finder->find('PES 2011');
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-            'test' => $fileContent,
+                    'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+                    'test' => $fileContent,
+                    'search' => $results,
         ]);
     }
+
 }
