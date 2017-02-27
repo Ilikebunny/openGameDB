7f6e0e6a025fc094512d9d3765d2483c1c6e4398
Ilikebunny <larrue.jerome@gmail.com>
2017-02-27 12:11:52 +0100

    Add jquery chosen

--- src/AppBundle/Form/GameFilterType.php
+++ src/AppBundle/Form/GameFilterType.php
@@ -5,25 +5,24 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 use Lexik\Bundle\FormFilterBundle\Filter\Form\Type as Filters;
 
+class GameFilterType extends AbstractType {
 
-class GameFilterType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('id', Filters\NumberFilterType::class)
-            ->add('title', Filters\TextFilterType::class)
-            ->add('releaseDate', Filters\DateFilterType::class)
-            ->add('players', Filters\NumberFilterType::class)
-            ->add('coop', Filters\BooleanFilterType::class)
-        
-            ->add('platform', Filters\EntityFilterType::class, array(
+                ->add('id', Filters\NumberFilterType::class)
+                ->add('title', Filters\TextFilterType::class)
+                ->add('releaseDate', Filters\DateFilterType::class)
+                ->add('players', Filters\NumberFilterType::class)
+                ->add('coop', Filters\BooleanFilterType::class)
+                ->add('platform', Filters\EntityFilterType::class, array(
                     'class' => 'AppBundle\Entity\Platform',
                     'choice_label' => 'name',
-            )) 
+                    'attr' => array(
+                        'class' => 'chosen-select'
+                    )
+                ))
 //            ->add('developers', Filters\EntityFilterType::class, array(
 //                    'choice_label' => 'name',
 //            )) 
@@ -35,27 +34,24 @@ class GameFilterType extends AbstractType
 //                    'class' => 'AppBundle\Entity\Genre',
 //                    'choice_label' => 'name',
 //            )) 
-            ->add('contentRatings', Filters\EntityFilterType::class, array(
+                ->add('contentRatings', Filters\EntityFilterType::class, array(
                     'class' => 'AppBundle\Entity\ContentRating',
                     'choice_label' => 'rating',
-            )) 
+                ))
         ;
         $builder->setMethod("GET");
-
-
     }
 
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return null;
     }
-    
-    public function configureOptions(OptionsResolver $resolver)
-    {
+
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'allow_extra_fields' => true,
             'csrf_protection' => false,
             'validation_groups' => array('filtering') // avoid NotBlank() constraint-related message
         ));
     }
+
 }
