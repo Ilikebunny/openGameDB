d692ec51d508def788d62802f77f78ccef8e1ab2
Ilikebunny <larrue.jerome@gmail.com>
2017-08-11 07:48:00 +0200

    Added searchbar type

--- src/AppBundle/Form/GameSearchType.php
+++ src/AppBundle/Form/GameSearchType.php
@@ -6,17 +6,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class GameSearchType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('search', TextType::class, array(
-                    'attr' => array(
-                        'placeholder' => 'Search game',
-                    ))
-                )
-                ->add('Search', SubmitType::class)
+        ->add('search', TextType::class, array(
+        'attr' => array(
+        'placeholder' => 'Search...',
+        ))
+        )
+        ->add('Type', ChoiceType::class, array(
+        'choices' => array(
+        'Game' => null,
+        'Platform' => true,
+        'Company' => false,
+        'People' => false,
+        )))
+        ->add('Search', SubmitType::class)
         ;
     }
 
